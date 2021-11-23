47c63a88035f784b6ea7822bd6f8f969baf14fc9
Михаил <89852226+BudkovMikhail@users.noreply.github.com>
2021-11-23 19:50:34 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,32 +1,54 @@
-$(document).ready(function() {
-    $(window).scroll(function() {
-        if ( $(this).scrollTop() > 800) {
-          $('.arrow-top').fadeIn(); 
-        } else {
-          $('.arrow-top').fadeOut();
-        }
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        speed: 1000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left_arrow.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right_arrow.svg"></button>'
     });
-    $('.arrow-top').on('click', function() {
-        $('html, body').stop().animate({scrollTop : 0}, 700);
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
     });
-});
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+    $('.button_catalog').each(function(i) 
+    {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
+    function valideForm(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                } 
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя!",
+                    minlength: jQuery.validator.format("Имя должно содержать минимум {0} симлова!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона!",
+                email: {
+                  required: "Нам нужен ваш адрес электронной почты, чтобы с вами связаться!",
+                  email: "Пожалуйста, введите корректный адрес!"
+                }
+            },
+        });
+    };
+    valideForm("#consultation-form");
+    valideForm("#consultation form");
+    valideForm("#order form");
 
-const hamburger = document.querySelector('.hamburger'),
-      menu = document.querySelector('.menu'),
-      closeElem = document.querySelector('.menu__close');
-
-hamburger.addEventListener('click', () => {
-    menu.classList.add('active');
-});
-
-closeElem.addEventListener('click', () => {
-    menu.classList.remove('active');
-});
-
-const counters = document.querySelectorAll('.rating__counter'),
-      lines = document.querySelectorAll('.rating__line');
-
-counters.forEach((item, i) => {
-    lines[i].style.width = item.innerHTML;
-});
-
+    $("input[name=phone]").mask("+38 (999) 999-99-99");
+});                                      
