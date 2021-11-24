e32fe20ee5334cc2fff3a4c8feea4fbbab921479
Михаил <89852226+BudkovMikhail@users.noreply.github.com>
2021-11-24 21:04:48 +0300

    Update smart.php

--- mailer/smart.php
+++ mailer/smart.php
@@ -18,7 +18,7 @@ $mail->Password = 'oirisesq1893';                           // Наш парол
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('', 'My portfolio');   // От кого письмо 
+$mail->setFrom('mishabudkov1893@gmail.com', 'My portfolio');   // От кого письмо 
 $mail->addAddress('ml.budkov@ukr.net');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
